ff36c2e735a110f12fc4f5e3107fe14c10c267a5
tmezouri <mezouri.thibault@gmail.com>
2019-09-10 16:57:33 +0200

    correction d'une érreur sur la page d'acceuil pour la validation w3c

--- view/homeView.php
+++ view/homeView.php
@@ -13,7 +13,17 @@
 		<h2 id="lastPostPreview" class="text-center">Dernière publication</h2>
 		<a id="postLink" href="index.php?action=post&amp;postId=<?= $data['id'] ?>"><h3><?= htmlspecialchars($data['title'])?></h3></a>
 		<em>Poster le <?= $data['publicationDate'] ?></em>
-		<p><?= substr($data['content'],0, strpos($data['content'],'</p>',0)) ?></div></p>
+		<?php
+		if(substr($data['content'], 0, 4) == '<div')
+		{
+			echo substr($data['content'], 0, strpos($data['content'],'</p>'));
+			echo substr($data['content'], strpos($data['content'],'</p>'), 4);
+			echo substr($data['content'], -6, 6);
+		}
+		else {
+			echo $data['content'];
+		}
+		?>
 		<a class="btn btn-dark" href="index.php?action=post&amp;postId=<?= $data['id'] ?>">Lire la suite</a>
 	</div>
 	<?php
